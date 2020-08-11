78a05110523b4dd4135ccf4fd42b07b60db15e64
Adriano M Pinheiro <adrianopinheiro.portugal@gmail.com>
2020-08-11 21:28:42 +0100

    teste

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 100
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) {
+    console.log(`MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança`) 
---
+++ ex009.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+var país = 'Brasil'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log('Brasileiro')
+} else {
+    console.log('Estrangeiro')
+}
